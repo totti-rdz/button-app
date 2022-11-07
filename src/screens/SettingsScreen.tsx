f230f6117fa491a318f3c3ab44db77ba0a6d92f1
totti-rdz <thorsten@achtsamkeitsakademie.de>
2022-11-07 16:59:11 +0100

    adjust SettingsScreen

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Input from "../components/Input";
 
 const SettingsScreen = () => {
+  const [input, setInput] = useState("");
+  const handleChange = (input: string) => setInput(input);
   return (
     <View style={styles.view}>
-      <Text>SettingsScreen</Text>
+      <Text style={styles.text}>Enter url:</Text>
+      <Input value={input} onChangeText={handleChange} />
     </View>
   );
 };
@@ -15,5 +20,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 8,
   },
 });
